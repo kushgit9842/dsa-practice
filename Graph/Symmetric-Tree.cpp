6f0e8fc6dfef6044dbb68d46075843c9c6fa85bd
kushagra kaushal <kishukaushal2004@gmail.com>
2026-05-14 21:41:11 +0530

    Added Symmetric-Tree in Graph

--- Graph/Symmetric-Tree.cpp
+++ Graph/Symmetric-Tree.cpp
@@ -5,4 +5,36 @@
 // Tags     : Tree, Depth-First Search, Breadth-First Search, Binary Tree
 // Date     : 2026-05-14
 // ─────────────────────────────────────────
- */\nclass Solution {\npublic:\n    bool check(TreeNode* l , TreeNode* r){\n        if (l==NULL && r==NULL){\n    }\n            return true;\n        }\n        if (l==NULL || r==NULL ){\n            return false;\n        }\n        if((l->val == r->val) && check(l->left,r->right) && check(l->right,r->left)){\n            return true; \n        }\n        return false;\n * };\n *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}\n *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}\n *     TreeNode() : val(0), left(nullptr), right(nullptr) {}\n *     TreeNode *right;\n[1,2,2,3,4,4,3]\n[1,2,2,null,3,null,3]\ntrue\nfalse\ntrue\nfalse\n
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool check(TreeNode* l , TreeNode* r){
+        if (l==NULL && r==NULL){
+            return true;
+        }
+        if (l==NULL || r==NULL ){
+            return false;
+        }
+        if((l->val == r->val) && check(l->left,r->right) && check(l->right,r->left)){
+            return true; 
+        }
+        return false;
+    }
+    bool isSymmetric(TreeNode* root) {
+        if (!root){
+            return true;
+        }
+        return check(root->left , root->right);
+        
+    }
+};
